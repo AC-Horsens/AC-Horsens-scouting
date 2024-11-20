3e4ef2f76f903e62a7e7a3af2aef3e5008ba4f1d
Seamus1992 <spbartholdy@gmail.com>
2024-11-20 10:09:29 +0100

    Update Scouting.py

--- Scouting.py
+++ Scouting.py
@@ -609,10 +609,14 @@ def Process_data(df_possession_xa,df_pv,df_matchstats,df_xg,squads):
                 (df_scouting['player_positionSide'].isin(['Right', 'Left']))
             ) |
             (
-                (df_scouting['player_position'].isin(['Attacking Midfielder', 'Striker'])) &
+                (df_scouting['player_position'].isin(['Attacking Midfielder', 'Striker'])) & 
                 (df_scouting['player_positionSide'].isin(['Right', 'Left']))
+            ) |
+            (
+                (df_scouting['player_position'] == 'Attacking Midfielder') & 
+                (df_scouting['player_positionSide'].str.contains('Right|Left', na=False))
             )
-        ]        
+        ]      
         df_10['minsPlayed'] = df_10['minsPlayed'].astype(int)
         df_10 = df_10[df_10['minsPlayed'].astype(int) >= minutter_kamp]
         df_10 = df_10[df_10['age_today'].astype(int) <= alder]
